a1e28efa704d613dc934354a395c2bcb798b5ac3
ishisora <tatsu.gami2573@gmail.com>
2024-12-17 01:49:26 +0900

    Add AtCoder

---
+++ atcoder/0/f.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+// #include <atcoder/all>
+// using namespace atcoder;
+
+using ll = long long;
+using ull = unsigned long long;
+using ldouble = long double;
+using LL = __int128_t;
+
+#define rep(i, k) for (ll i = 0; i < (ll)(k); i++)
+#define repR(i, k) for (ll i = k - 1; i >= 0; i--)
+#define all(v) v.begin(), v.end()
+
+const ll INF = 2e18;  // INF+INFがオーバフローしない程度の大きい値
+
+int main() {}
---
+++ atcoder/atcoder-library.md
@@ -0,0 +1,18 @@
+## AtCoder Library の使い方
+
+https://atcoder.github.io/ac-library/master/document_ja/ に書いてある。
+
+### AtCoder Library を使って a.cpp をコンパイルする場合
+
+下記のリポジトリに置いてある atcoder ディレクトリをカレントディレクトリ下に配置して次のコマンドを実行する。
+
+* リポジトリ: https://github.com/atcoder/ac-library
+
+```
+g++ a.cpp -std=c++20 -I .を実行
+```
+
+### 参考文献
+
+- https://atcoder.github.io/ac-library/master/document_ja/
+- https://github.com/atcoder/ac-library
